2888bb705605988d0b4c6aa4954d806af4aa3c55
Crowdbotics <team@crowdbotics.com>
2021-04-26 01:14:17 +0000

    Update admin title

--- backend/project_citizens_25936/urls.py
+++ backend/project_citizens_25936/urls.py
@@ -35,9 +35,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "Project Citizens"
-admin.site.site_title = "Project Citizens Admin Portal"
-admin.site.index_title = "Project Citizens Admin"
+admin.site.site_header = "Project"
+admin.site.site_title = "Project Admin Portal"
+admin.site.index_title = "Project Admin"
 
 # swagger
 api_info = openapi.Info(
